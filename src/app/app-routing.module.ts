0543c3ec192613500985bbcba8b520513b39c0c2
wangonya <kwangonya@gmail.com>
2019-08-13 20:07:37 +0300

    add router paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {SigninComponent} from './signin/signin.component';
+import {SignupComponent} from './signup/signup.component';
+import {LostComponent} from './lost/lost.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '**', component: LostComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
