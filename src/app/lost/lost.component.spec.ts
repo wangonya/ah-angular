e4d4887393779061694113da0ea47b043b597ebf
wangonya <kwangonya@gmail.com>
2019-08-14 07:58:06 +0300

    add lost component tests

--- src/app/lost/lost.component.spec.ts
+++ src/app/lost/lost.component.spec.ts
@@ -1,13 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { NzResultModule } from 'ng-zorro-antd/result';
 
 import { LostComponent } from './lost.component';
 
 describe('LostComponent', () => {
   let component: LostComponent;
   let fixture: ComponentFixture<LostComponent>;
+  let element;
+  let button;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ NzResultModule ],
       declarations: [ LostComponent ]
     })
     .compileComponents();
@@ -17,9 +21,17 @@ describe('LostComponent', () => {
     fixture = TestBed.createComponent(LostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    element = fixture.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Back Home button', () => {
+
+    beforeEach(() => {
+      button = element.querySelector('button');
+    });
+
+    it(`should exist with 'Back Home' as text`, () => {
+      expect(button.textContent).toBe('Back Home');
+    });
   });
 });
