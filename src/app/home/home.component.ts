83773ca112d0703f22f9f448245a88b0e35b7d1c
wangonya <kwangonya@gmail.com>
2019-08-15 11:24:05 +0300

    load articles from api

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService, MessageService } from '../_services';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  articles;
+  defaultImg = 'https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png';
+
+  constructor(private apiService: ApiService, private messageService: MessageService) { }
 
   ngOnInit() {
+    this.apiService.getArticles().subscribe((data) => {
+      this.messageService.createMessage('success', 'Success');
+      this.articles = data['results'];
+      console.log(this.articles);
+    });
   }
-
 }
