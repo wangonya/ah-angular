4b5113f80cd2738f59b1cca911cd8d7a00fb0f3e
wangonya <kwangonya@gmail.com>
2019-08-17 21:40:13 +0300

    setu signin

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../_models';
+import {ApiService} from '../_services';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.less']
 })
 export class SigninComponent implements OnInit {
+  user: User = new User();
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    this.apiService.postRequest('users/login', {user: this.user}).subscribe(
+      data => console.log(data),
+      error => console.log('Error logging in', error.message));
   }
 
 }
